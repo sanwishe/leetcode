218abf8c642729a590457aa5c40cb9abc70bb38b
Marvin <jiangmingzhi.23@163.com>
2018-09-27 19:54:50 +0800

    Create SolutionTest.java

---
+++ src/test/java/io/jmz/leetcode/junior/longestPalindrome/SolutionTest.java
@@ -0,0 +1,20 @@
+package io.jmz.leetcode.junior.longestPalindrome;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "cbbd, bb",
+            "babad, bab",
+    })
+    void longestPalindrome(String given, String expect) {
+        String act = new Solution().longestPalindrome(given);
+
+        assertEquals(act, expect);
+    }
+}
