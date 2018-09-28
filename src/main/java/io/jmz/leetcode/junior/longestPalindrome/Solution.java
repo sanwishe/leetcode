ff4fa3757b19c12b467310da768eeb32bb4765a4
Marvin <jiangmingzhi.23@163.com>
2018-09-28 10:59:26 +0800

    Update Solution.java

--- src/main/java/io/jmz/leetcode/junior/longestPalindrome/Solution.java
+++ src/main/java/io/jmz/leetcode/junior/longestPalindrome/Solution.java
@@ -2,7 +2,6 @@ package io.jmz.leetcode.junior.longestPalindrome;
 
 public class Solution {
     public String longestPalindrome(String s) {
-
         if (null == s || s.isEmpty()) {
             return s;
         }
@@ -10,20 +9,21 @@ public class Solution {
         int palindromeLen = 0;
         String palindrome = "";
         for (int i = 0; i < len; i++) {
+            outter:
             for (int j = len - 1; j > i; j--) {
                 if (s.charAt(i) != s.charAt(j)) {
                     continue;
                 }
 
-                for (int m = 0, n = j; m + i <= n; ++m, --n) {
-                    if (s.charAt(i + m) != s.charAt(n)) {
+                for (int m = i, n = j; m <= n; ++m, --n) {
+                    if (s.charAt(m) != s.charAt(n)) {
                         break;
                     }
 
-                    if ((s.charAt(i + m) == s.charAt(n)) && (n - m - i <= 2) && (j - i + 1 > palindromeLen)) {
+                    if ((s.charAt(m) == s.charAt(n)) && (n - m <= 2) && (j - i + 1 > palindromeLen)) {
                         palindromeLen = j - i + 1;
                         palindrome = s.substring(i, j + 1);
-                        break;
+                        break outter;
                     }
                 }
             }
